e8a6d3421ac1713980fdeedb8131109339da1495
Luis Fernando Rogel Rosado <lrogel@marista.edu.mx>
2025-03-07 13:06:43 -0600

    Estructura del Proyecto Inicial

---
+++ web/src/test/java/gob/yucatan/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package gob.yucatan.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
